d2ff438a104feceb6b41d687da2a34716483f734
AHerrera23 <herreraabel892@gmail.com>
2025-01-04 19:08:55 -0300

    seccion 3 y btn mapa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("its online");
+console.log("its online")
+let btnMapa = document.getElementById("mapa")
+// funcion de mapa
+btnMapa.addEventListener("click",()=>{
+    console.log("funciona")
+    const urlMapa ="https://maps.app.goo.gl/EVLk4wQqHDyFo6su7"
+    window.open(urlMapa, "_blanck")
+});
