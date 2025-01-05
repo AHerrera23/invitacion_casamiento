6a05506a63c8ec1e635016de42c981d7d4990b36
AHerrera23 <herreraabel892@gmail.com>
2025-01-04 21:17:38 -0300

    carrucel de imagenes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ btnMapa.addEventListener("click",()=>{
     const urlMapa ="https://maps.app.goo.gl/EVLk4wQqHDyFo6su7"
     window.open(urlMapa, "_blanck")
 });
+
+// funcion del carrucel
+let currentIndex = 0
+
+function moveSlide(direccion){
+    const slides = document.querySelectorAll('.carrucel-inner img')
+    const totalSlides = slides.length
+
+    currentIndex += direccion
+
+    if(currentIndex <0){
+        currentIndex = totalSlides -1
+    } else if(currentIndex >= totalSlides){
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100
+    document.querySelector('.carrucel-inner').style.transform =  `translateX(${offset}%)`
+}
